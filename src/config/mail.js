32f8ee62163c3f259d6cbe69647e29c67cfebb37
netohelvecio <helvecioneto77@gmail.com>
2019-10-30 20:54:01 -0300

    configurando nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '238d2931ec5ce0',
+    pass: '197ae75accc348',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gorbarber.com>',
+  },
+};
